ecccb6ebf830c43e7b9f15d93e95e410f9e80424
rharper24 <ronharper938@gmail.com>
2025-08-19 10:44:05 -0500

    "Configure CORS middleware with custom origins and settings"

--- config/middlewares.js
+++ config/middlewares.js
@@ -2,7 +2,18 @@ module.exports = [
   'strapi::logger',
   'strapi::errors',
   'strapi::security',
-  'strapi::cors',
+  {
+    name: 'strapi::cors',
+    config: {
+      origin: [
+        'https://www.clarkcoldvisuals.com',
+        'http://localhost:3000',
+      ],
+      methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'HEAD', 'OPTIONS'],
+      headers: ['Content-Type', 'Authorization', 'Origin', 'Accept'],
+      keepHeaderOnError: true,
+    },
+  },
   'strapi::poweredBy',
   'strapi::query',
   'strapi::body',
